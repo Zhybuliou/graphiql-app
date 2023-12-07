f8bbf7b62796cac90ff74e6a69026e75f50af032
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-12-07 15:33:30 +0300

    feat: add basic ErrorBoundary, Suspense and lazy loading

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,25 +1,33 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Suspense } from 'react';
 import { privateRoutes, publicRoutes } from './routes';
 import RouteParths from '../types/enums/routeParths';
 
 function AppRouter() {
   const user = false;
-  return user ? (
-    <Routes>
-      {privateRoutes.map(({ path, Page }) => (
-        <Route key={path} path={path} element={<Page />} />
-      ))}
-    </Routes>
-  ) : (
-    <Routes>
-      {publicRoutes.map(({ path, Page }) => (
-        <Route key={path} path={path} element={<Page />} />
-      ))}
-      <Route
-        path={RouteParths.MAIN}
-        element={<Navigate replace to={RouteParths.WELCOME} />}
-      />
-    </Routes>
+  return (
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <Suspense fallback={<p>Loading...</p>}>
+        {user ? (
+          <Routes>
+            {privateRoutes.map(({ path, Page }) => (
+              <Route key={path} path={path} element={<Page />} />
+            ))}
+          </Routes>
+        ) : (
+          <Routes>
+            {publicRoutes.map(({ path, Page }) => (
+              <Route key={path} path={path} element={<Page />} />
+            ))}
+            <Route
+              path={RouteParths.MAIN}
+              element={<Navigate replace to={RouteParths.WELCOME} />}
+            />
+          </Routes>
+        )}
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
