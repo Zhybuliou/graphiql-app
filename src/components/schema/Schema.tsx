a8ab5001e879c63efd8403d27b806840b19424cb
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-12-12 16:48:36 +0300

    feat: add schema query

--- src/components/schema/Schema.tsx
+++ src/components/schema/Schema.tsx
@@ -2,22 +2,14 @@
 
 import React, { useState } from 'react';
 import Button from '../ui/Button';
+import SCHEMA_QUERY from './schemaQuery';
 
 const API_URL = 'https://rickandmortyapi.com/graphql';
-
-const DEF_REQUEST = `{
-  __schema {
-    types {
-      name
-      description
-    }
-  }
-}`;
 
 function Schema() {
   const [apiUrl, setApiUrl] = useState<string>(API_URL);
   const [apiResponse, setApiResponse] = useState<string>('');
-  const [apiRequest, setApiRequest] = useState<string>(DEF_REQUEST);
+  const [apiRequest, setApiRequest] = useState<string>(SCHEMA_QUERY);
 
   async function makeRequest(query: string) {
     const response = await fetch(API_URL, {
@@ -29,8 +21,7 @@ function Schema() {
       body: JSON.stringify({ query }),
     });
     const data = await response.json();
-    console.log(data);
-    setApiResponse(JSON.stringify(data));
+    setApiResponse(JSON.stringify(data, null, 2));
   }
 
   return (
