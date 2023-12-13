f67d0ba9e5b2700e3f69aab2972ead6e9438e3f8
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-12-13 16:29:52 +0300

    refactor: refactor print query endpoints and add print type buttons

--- src/components/schema/Schema.tsx
+++ src/components/schema/Schema.tsx
@@ -1,13 +1,8 @@
 /* eslint-disable no-console */
 
-import React, { useState } from 'react';
-import {
-  buildClientSchema,
-  // GraphQLList,
-  // GraphQLObjectType,
-  GraphQLSchema,
-  printSchema,
-} from 'graphql';
+import React, { useEffect, useState } from 'react';
+import { buildClientSchema, GraphQLSchema, printType } from 'graphql';
+
 import Button from '../ui/Button';
 import SCHEMA_QUERY from './schemaQuery';
 
@@ -16,6 +11,7 @@ const API_URL = 'https://rickandmortyapi.com/graphql';
 function Schema() {
   const [apiUrl, setApiUrl] = useState<string>(API_URL);
   const [apiRequest, setApiRequest] = useState<string>(SCHEMA_QUERY);
+  const [result, setResult] = useState<string>('');
   const [clientSchema, setClientSchema] = useState<GraphQLSchema | null>(null);
 
   async function makeRequest(query: string) {
@@ -28,29 +24,22 @@ function Schema() {
       body: JSON.stringify({ query }),
     });
     const schema = await response.json();
-    console.log(schema.data);
+    // console.log(schema.data);
     setClientSchema(buildClientSchema(schema.data));
   }
 
-  function getEndpoints() {
-    if (!clientSchema) return '';
-    return printSchema(clientSchema);
-
-    // const query = clientSchema.getQueryType();
-    // if (!query) return '';
-    // const mapOfEndpoints = query.getFields();
-    // return Object.values(mapOfEndpoints).map((endpoint) => {
-    //   const { name, description, type } = endpoint;
-    //   let typeName = 'NO_TYPE!!!';
-    //   if (type instanceof GraphQLObjectType) {
-    //     typeName = type.name;
-    //   }
-    //   if (type instanceof GraphQLList) {
-    //     typeName = type.ofType.toString();
-    //   }
-    //   return `${name} : ${typeName} === ${description}\n`;
-    // });
-  }
+  useEffect(() => {
+    if (!clientSchema) {
+      setResult('!clientSchema');
+      return;
+    }
+    const query = clientSchema.getQueryType();
+    if (!query) {
+      setResult('!query');
+      return;
+    }
+    setResult(() => printType(query));
+  }, [clientSchema]);
 
   return (
     <div className="flex flex-col w-full gap-3">
@@ -71,10 +60,24 @@ function Schema() {
         Get Schema
       </Button>
       {clientSchema && (
+        <div>
+          {Object.values(clientSchema.getTypeMap()).map((type) => {
+            return (
+              <Button
+                key={type.name}
+                onClick={() => setResult(printType(type))}
+              >
+                {type.name}
+              </Button>
+            );
+          })}
+        </div>
+      )}
+      {clientSchema && (
         <textarea
           className="border-2 border-black"
           rows={10}
-          defaultValue={getEndpoints()}
+          value={result}
           readOnly
         />
       )}
