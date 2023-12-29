99ceb24c17076e3857c592a7b24bac5e8585e8f4
Space Pocket <space1985pocket@gmail.com>
2023-12-29 08:15:02 +0300

    refactor: add icons for input pass

--- src/components/ui/UIFormInput.tsx
+++ src/components/ui/UIFormInput.tsx
@@ -1,6 +1,8 @@
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
-import React from 'react';
+import React, { useState } from 'react';
+import IconView from './icons/IconView';
+import IconNoView from './icons/IconNoView';
 
 type UiFormInputProps<T extends FieldValues> = {
   type: React.HTMLInputTypeAttribute;
@@ -19,10 +21,15 @@ function UIFormInput<T extends FieldValues>({
   placeholder,
   error = '',
 }: UiFormInputProps<T>) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <label htmlFor={name} className="relative w-80 mb-6 text-black">
       <input
-        type={type}
+        type={showPassword ? 'text' : type}
         id={name}
         className={`p-4 text-base w-full rounded-xl border border-gray-200 focus:outline-none focus:border-blue-500 ${
           error ? ' border-red-400' : 'border-gray-200'
@@ -30,6 +37,19 @@ function UIFormInput<T extends FieldValues>({
         {...register(name, { required })}
         placeholder={placeholder}
       />
+      {type === 'password' && (
+        <span
+          aria-hidden="true"
+          className="absolute top-5 right-5 cursor-pointer"
+          onClick={handleTogglePassword}
+        >
+          {showPassword ? (
+            <IconView className="w-4 h-4" />
+          ) : (
+            <IconNoView className="w-4 h-4" />
+          )}
+        </span>
+      )}
       {error && <div className="text-red-400 select-none">{error}</div>}
     </label>
   );
