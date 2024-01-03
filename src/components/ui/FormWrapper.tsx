9b240b168552f49fe8d51fa2c1559c531d4c05cc
spacepocket1985 <107361757+spacepocket1985@users.noreply.github.com>
2024-01-03 21:46:56 +0300

    Refactor/update SignIn, SignUp styles and add tests (#53)
    * refactor: update sign-in and sign-up styles
    
    * refactor: update toast.error msg
    
    * refactor: add icons for input pass
    
    * refactor: update validation shema,unicode symbols
    
    * refactor: update errors fileds
    
    * refactor: add first test
    
    * refactor: add third test for sign-in page
    
    * refactor: add tests for SignUpPage
    
    * refactor: update tests for SignInPage
    
    * refactor: update tests for signIn
    
    ---------
    
    Co-authored-by: Space Pocket <space1985@pocket@gmail.com>

--- src/components/ui/FormWrapper.tsx
+++ src/components/ui/FormWrapper.tsx
@@ -3,9 +3,7 @@ import { PropsWithChildren } from '../../types/utilityTypes';
 
 function FormWrapper({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col items-center p-10 text-center bg-gray-400 rounded-lg">
-      {children}
-    </div>
+    <div className="flex flex-col items-center text-center">{children}</div>
   );
 }
 
---
+++ src/pages/signInPage.test.tsx
@@ -0,0 +1,113 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { LocaleProvider } from '../context/local';
+import * as firebase from '../firebase/firebase';
+
+import SignInPage from './SignInPage';
+import AppRouter from '../routes/AppRouter';
+
+test('renders sign in page', () => {
+  render(
+    <LocaleProvider>
+      <MemoryRouter>
+        <SignInPage />
+      </MemoryRouter>
+    </LocaleProvider>
+  );
+  const signInHeader = screen.getByText(/Sign In, please/i);
+  expect(signInHeader).toBeInTheDocument();
+});
+
+test('displays an error message if the email is incorrect', async () => {
+  render(
+    <LocaleProvider>
+      <MemoryRouter>
+        <SignInPage />
+      </MemoryRouter>
+    </LocaleProvider>
+  );
+
+  const emailInput = screen.getByPlaceholderText(
+    /E-mail Address/i
+  ) as HTMLInputElement;
+
+  fireEvent.change(emailInput, { target: { value: 'invalidMailexample.com' } });
+
+  await waitFor(() => {
+    expect(screen.getByText(/Invalid email address/i)).toBeInTheDocument();
+  });
+});
+
+test('checking submit button status', async () => {
+  render(
+    <LocaleProvider>
+      <MemoryRouter>
+        <SignInPage />
+      </MemoryRouter>
+    </LocaleProvider>
+  );
+
+  const emailInput = screen.getByPlaceholderText(
+    /E-mail Address/i
+  ) as HTMLInputElement;
+  const passwordInput = screen.getByPlaceholderText(
+    'Password'
+  ) as HTMLInputElement;
+  const submitButton = screen.getByText(/Login/i) as HTMLButtonElement;
+
+  fireEvent.change(emailInput, { target: { value: 'testMail@test.com' } });
+  fireEvent.change(passwordInput, { target: { value: 'Qwerty1234%' } });
+
+  await waitFor(() => {
+    expect(submitButton).not.toHaveAttribute('disabled');
+  });
+});
+
+test('should call logInWithEmailAndPassword when form is submitted', async () => {
+  const spy = vi.spyOn(firebase, 'logInWithEmailAndPassword');
+
+  render(
+    <LocaleProvider>
+      <MemoryRouter>
+        <AppRouter />
+        <SignInPage />
+      </MemoryRouter>
+    </LocaleProvider>
+  );
+
+  const emailInput = screen.getByPlaceholderText(
+    /E-mail Address/i
+  ) as HTMLInputElement;
+  const passwordInput = screen.getByPlaceholderText(
+    'Password'
+  ) as HTMLInputElement;
+
+  const submitButton = screen.getByText(/Login/i) as HTMLButtonElement;
+  await waitFor(() => {
+    fireEvent.change(emailInput, { target: { value: 'testMail@test.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'Qwerty1234%' } });
+  });
+
+  fireEvent.click(submitButton);
+  await waitFor(() => {
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('testMail@test.com', 'Qwerty1234%');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
+
+test('navigates to SignUpPage when "Register" link is clicked', () => {
+  render(
+    <LocaleProvider>
+      <BrowserRouter>
+        <AppRouter />
+        <SignInPage />
+      </BrowserRouter>
+    </LocaleProvider>
+  );
+  fireEvent.click(screen.getByText('Register'));
+
+  expect(window.location.pathname).toBe('/sign-up');
+});
