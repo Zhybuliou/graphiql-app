10d744d79715eaa78c2cf381512c69177e822b2b
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-05 20:09:54 +0300

    feat: check test

--- src/pages/page404/page404.test.tsx
+++ src/pages/page404/page404.test.tsx
@@ -1,13 +1,11 @@
 import '@testing-library/jest-dom';
 import { expect } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Page404 from './Page404';
 
 describe('Check render page 404', () => {
   it('check show text Not Found', () => {
     render(<Page404 />);
-    waitFor(() => {
-      expect(screen.getByText(/154edd/i)).toBeInTheDocument();
-    });
+    expect(screen.getByText(/154edd/i)).toBeInTheDocument();
   });
 });
