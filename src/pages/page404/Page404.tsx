bb863389ef205b72305f96df8f58bd4d772cdbc9
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-01-05 21:54:12 +0300

    feat: add tests

--- src/pages/page404/Page404.tsx
+++ src/pages/page404/Page404.tsx
@@ -1,9 +1,11 @@
 import { PageWrapper } from '../../components/ui/PageWrapper';
+import { useLocale } from '../../context/local';
 
 function Page404() {
+  const { state } = useLocale();
   return (
     <PageWrapper>
-      <h1>Not Found</h1>
+      <h1>{state.strings.page404Tittle}</h1>
     </PageWrapper>
   );
 }
